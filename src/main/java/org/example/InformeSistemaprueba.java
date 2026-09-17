8f2dd0e47e035ea90aac40c253bd4fb6ca5721a3
DIEGO CURRÁS <diegocurrasrodriguez@gmail.com>
2026-09-17 14:33:04 +0200

    Parte 1 completada

--- src/main/java/org/example/InformeSistemaprueba.java
+++ src/main/java/org/example/InformeSistemaprueba.java
@@ -1,9 +1,7 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void Main(){
+public class InformeSistemaprueba {
+    static void Main(String[] args){
         Runtime runtime = Runtime.getRuntime();
         long[] reservado = new long[8 * 1024 * 1024];
 
